e727ffd7a3fdd7be0a80a58ae293b0b54d38c66e
Papri <papri.sen2020@gmail.com>
2025-09-13 11:32:09 +0530

    Create Exp13_1_ConstructorOverloading1.cpp

---
+++ Exp13_1_ConstructorOverloading1.cpp
@@ -0,0 +1,30 @@
+// Papri Sen
+// 24070123070
+// A3
+
+#include <iostream>
+using namespace std;
+class Add{
+public:
+    Add(int a, int b){
+        cout << "a + b = "<<a+b<<endl;
+    }
+    Add(float a, float b){
+        cout << "Float a + b = "<<a+b<<endl;
+    }
+    Add(int a, int b,int c){
+        cout << "a + b + c = "<<a+b+c<<endl;
+    }
+};
+int main() {
+    Add a(18,16);
+    Add b(10.25f,11.36f);
+    Add c(10,20,30);
+    return 0;
+}
+/*
+Output:
+a + b = 34
+Float a + b = 21.61
+a + b + c = 60
+*/
